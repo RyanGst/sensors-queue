8693851a40c74cc0f615f1dfc59cd652309ed581
RyanGst <ryangst.hire@gmail.com>
2025-05-31 18:52:37 -0300

    chore: remove fake_reader.py as it is no longer needed for sensor reading logic

--- src/fake_reader.py
+++
@@ -1,9 +0,0 @@
-import logging
-import random
-
-logging.basicConfig(level=logging.INFO)
-
-logging.info("Fake Temperature and Humidity Sensor")
-
-def read_fake_sensor() -> tuple[float, float]:
-    return random.randint(20, 30), random.randint(40, 50)
